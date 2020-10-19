242cdbfd455a67eec74e4b5439ac41ef5e30bc97
reena kumari <mehta.reena08@gmail.com>
2020-10-19 11:57:08 +0530

    Add files via upload

---
+++ Reader_text_audio.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Oct 19 11:23:39 2020
+
+@author: reena
+"""
+
+#!pip install pyttsx3
+#!pip install PyPDF2
+
+import pyttsx3
+import PyPDF2
+
+book = open('D:\ineuron\interview-question-data-science--master\Data Science Interview Interview Questions(#Day28).pdf','rb')
+bookReader = PyPDF2.PdfFileReader(book)
+print(bookReader.getNumPages())
+#pagenumber=bookReader.getNumPages()
+page=''
+for pages in range(bookReader.getNumPages()):
+    page = bookReader.getPage(pages) #bookReader.getPage(14) 
+    text = page.extractText()
+    print(text)
+    output = pyttsx3.init()
+    output.say(text)
+    output.runAndWait()
